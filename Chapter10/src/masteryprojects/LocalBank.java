e818335e9def297d9f4ad0ab54aa33502c888578
27165005 <27165005@L815WD220008.edu.cbe.ab.ca>
2025-02-21 11:17:15 -0700

    gg

--- Chapter10/src/masteryprojects/LocalBank.java
+++ Chapter10/src/masteryprojects/LocalBank.java
@@ -1,6 +1,7 @@
 package masteryprojects;
 
 import java.awt.EventQueue;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 
@@ -29,14 +30,47 @@ public class LocalBank {
 	 */
 	public LocalBank() {
 		initialize();
-	}
+		Bank easySave = new Bank();
+		 Scanner input = new Scanner(System.in);
+		 String action, acctID;
+		 Double amt;
 
+		 /* display menu of choices */
+		 do {
+		 System.out.println("\nDeposit\\Withdrawal\\Check balance");
+		 System.out.println("Add an account\\Remove an account");
+		 System.out.println("Quit\n");
+		 System.out.print("Enter choice: ");
+		 action = input.next();
+
+		 if (action.equalsIgnoreCase("A")) {
+		 easySave.addAccount();
+		 } else if (!action.equalsIgnoreCase("Q")) {
+		 System.out.print("Enter account ID: ");
+		 acctID = input.next();
+		 if (action.equalsIgnoreCase("D")) {
+		 System.out.print("Enter deposit amount: ");
+		 amt = input.nextDouble();
+		 easySave.transaction(1, acctID, amt);
+		 } else if (action.equalsIgnoreCase("W")) {
+		 System.out.print("Enter withdrawal amount: ");
+		 amt = input.nextDouble();
+		 easySave.transaction(2, acctID, amt);
+		 } else if (action.equalsIgnoreCase("C")) {
+		 easySave.checkBalance(acctID);
+		 } else if (action.equalsIgnoreCase("R")) {
+		 easySave.deleteAccount(acctID);
+		 }
+		 }
+		 } while (!action.equalsIgnoreCase("Q"));
+		 }
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 820, 522);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
